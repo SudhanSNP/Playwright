c4b67351518db1ec9c21768962b80ce0c136ae7f
Sudhan Sankara Narayana Pillai <ssankaranarayanapi@corelogic.com>
2023-06-14 16:37:51 +0530

    Added home task 1

---
+++ Playwright_Training/tests/training.test.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.google.com/');
+  await page.frameLocator('iframe[name="callout"]').getByRole('button', { name: 'No thanks' }).click();
+  await page.getByRole('combobox', { name: 'Search' }).click();
+  await page.getByRole('combobox', { name: 'Search' }).fill('playwright');
+  await page.getByRole('combobox', { name: 'Search' }).press('Enter');
+  await page.getByRole('link', { name: 'Playwright: Fast and reliable end-to-end testing for modern ... Playwright https://playwright.dev' }).click();
+});
